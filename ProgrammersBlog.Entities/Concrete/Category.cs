a3686482fda66bc18a27b419a94fd6355e53ae70
ofaruk84 <korkmazfaruk84@gmail.com>
2022-02-27 03:36:12 +0300

    Add project files.

---
+++ ProgrammersBlog.Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProgrammersBlog.Shared.Abstract;
+
+namespace ProgrammersBlog.Entities.Concrete
+{
+    public class Category : EntityBase,IEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+
+
+    }
+}
---
+++ ProgrammersBlog.Shared/Abstract/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Abstract
+{
+    public abstract class EntityBase
+    {
+        public virtual  int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual bool IsDeleted { get; set; }
+        public virtual  bool IsActive { get; set; }
+        public virtual string CreatedByName { get; set; } = "Admin";
+        public virtual string ModifiedByName { get; set; } = "Admin";
+        public string Note { get; set; }
+    }
+}
